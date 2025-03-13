a8604f3bdd70e3b2834bd2d885bfd87ca423b97f
vitorluisbc1 <vitorluispte@gmail.com>
2025-03-13 02:03:02 +0000

    cartassupertrunfodesafio1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 int main() {
 
-// Carta 1 - variáveis
+// Carta 1 - variaveis
 
 char estado1;
- int população1;
+ int populacao1;
 float area1, pib1;
-int pontosturísticos1;
+int pontosturisticos1;
 
-//Carta 1 - variáveis Entrada de dados - Carta 1
+//Entrada de dados - Carta 1 
 
 printf("Digite o estado da Carta 1 (A-H):\n");
-scanf("%c, &estado1");
-printf("Digite a população:\n");
-scanf("%d, &população1");
-printf("digite a área (km²):\n");
+scanf("%c", &estado1);
+printf("Digite a populacao:\n");
+scanf("%d", &populacao1);
+printf("digite a area (km²):\n");
 scanf("%f", &area1);
 printf("Digite o PIB:\n");
-scanf("%f, &pib1");
-printf("Digite a quantidade de pontos turísticos:\n");
-scanf("%d, &pontoturistico1");
+scanf("%f", &pib1);
+printf("Digite a quantidade de pontos turisticos:\n");
+scanf("%d", &pontosturisticos1);
 
 
-// Carta 2
+// Carta 2 - variaveis
 char estado2;
-int população2;
+int populacao2;
 float area2, pib2;
-int pontosturísticos2;
+int pontosturisticos2;
 
-//Carta 2 - variáveis Entrada de dados - Carta 2
+// Entrada de dados - Carta 2
 
 printf("Digite o estado da Carta 2 (A-H):\n");
-scanf("%c, &estado2");
-printf("Digite a população:\n");
-scanf("%d, &população2");
+scanf("%c", &estado2);
+printf("Digite a populacao:\n");
+scanf("%d", &populacao2);
 printf("digite a área (km²):\n");
 scanf("%f", &area2);
 printf("Digite o PIB2:\n");
-scanf("%f, &pib2");
+scanf("%f", &pib2);
 printf("Digite a quantidade de pontos turísticos:\n");
-scanf("%d, &pontoturistico2");
+scanf("%d", &pontosturisticos2);
 
 return 0;
 }
