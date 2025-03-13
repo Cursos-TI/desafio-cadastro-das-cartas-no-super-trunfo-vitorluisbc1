e8ba4273f856a0e9e3c03c225989158bf46ad4f9
vitorluisbc1 <vitorluispte@gmail.com>
2025-03-13 01:41:26 +0000

    cartasquaseterminadas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+// Carta 1 - variáveis
+
+char estado1;
+ int população1;
+float area1, pib1;
+int pontosturísticos1;
+
+//Carta 1 - variáveis Entrada de dados - Carta 1
+
+printf("Digite o estado da Carta 1 (A-H):\n");
+scanf("%c, &estado1");
+printf("Digite a população:\n");
+scanf("%d, &população1");
+printf("digite a área (km²):\n");
+scanf("%f", &area1);
+printf("Digite o PIB:\n");
+scanf("%f, &pib1");
+printf("Digite a quantidade de pontos turísticos:\n");
+scanf("%d, &pontoturistico1");
+
+
+// Carta 2
+char estado2;
+int população2;
+float area2, pib2;
+int pontosturísticos2;
+
+//Carta 2 - variáveis Entrada de dados - Carta 2
+
+printf("Digite o estado da Carta 2 (A-H):\n");
+scanf("%c, &estado2");
+printf("Digite a população:\n");
+scanf("%d, &população2");
+printf("digite a área (km²):\n");
+scanf("%f", &area2);
+printf("Digite o PIB2:\n");
+scanf("%f, &pib2");
+printf("Digite a quantidade de pontos turísticos:\n");
+scanf("%d, &pontoturistico2");
+
+return 0;
 }
